de912fc401f6112ce2c6ff3009d99621dd750e18
H S <sekhah@my.lanecc.edu>
2019-11-30 21:23:06 -0800

    New views and view changes.

--- Planets/Controllers/HomeController.cs
+++ Planets/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Planets.Models;
 
@@ -11,47 +12,57 @@ namespace Planets.Controllers
         // Dependency injection
         public HomeController(IRepository repository) => _repository = repository;
 
-        public IActionResult Index() => View();
+        // We need the best three most recent surveys on the home page
+        public IActionResult Index() => View(_repository.Surveys.Where(x => x.Rating > 1).OrderByDescending(x => x.SurveyDateTime).Take(4));
 
+        // Pages for the planets, etc
         public IActionResult Mercury() => View();
-
         public IActionResult Venus() => View();
-
         public IActionResult Earth() => View();
-
         public IActionResult Mars() => View();
-
         public IActionResult Jupiter() => View();
-
         public IActionResult Saturn() => View();
-
         public IActionResult Uranus() => View();
-
         public IActionResult Neptune() => View();
-
         public IActionResult TakeSurvey() => View();
+        public IActionResult ViewSurveys() => View();
+        public IActionResult About() => View();
 
+        // Pages without Nav Links
         public IActionResult ThankSurvey() => View();
+        public IActionResult SearchSurveys() => View();
+        public IActionResult WrongPassword() => View();
 
         [HttpPost]
         public IActionResult TakeSurvey(string firstName, string lastName, string city, string country, string rating, string comment)
         {
-            Survey survey = new Survey
+            // First we add the survey to the repository
+            _repository.AddSurvey(new Survey
             {
                 FirstName = firstName,
                 LastName = lastName,
                 City = city,
                 Country = country,
-                Rating = rating == "" ? 5: Int32.Parse(rating),
+                Rating = rating == "" ? 5 : Int32.Parse(rating),
                 Comment = comment,
                 SurveyDateTime = DateTime.Now
+            });
+
+            // Now we create a message to thank him or her based on the rating
+            var message = rating switch
+            {
+                "1" => "one star! What have we done to deserve this?",
+                "5" => "five stars! Thank you so much! You will now be mentioned on our home page!",
+                _ => (rating + " stars.")
             };
-            _repository.AddSurvey(survey);
-            return View("ThankSurvey", survey);
+
+            // Since we are allowed to send only one variable to the view, we will use a Tuple to send many
+            var thankUser = new Tuple<string, string, string, string>(firstName, message, city, country);
+
+            return View("ThankSurvey", thankUser);
         }
 
-        public IActionResult ViewSurveys() => View();
-
-        public IActionResult About() => View();
+        [HttpPost]
+        public IActionResult ViewSurveys(string password) => password == "password" ? View("SearchSurveys") : View("WrongPassword");
     }
 }
