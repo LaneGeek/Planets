ee8eba3ac2e818c0f817e0a059a0631a6a453328
H S <sekhah@my.lanecc.edu>
2019-12-04 20:07:46 -0800

    Added Unit Tests project.

---
+++ Planets.Tests/SurveyTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Planets.Controllers;
+using Planets.Models;
+using Xunit;
+
+namespace Planets.Tests
+{
+    public class SurveyTests
+    {
+        public IRepository Repository = new FakeRepository();
+
+        public SurveyTests()
+        {
+            // Fill the empty repository with data for testing
+        }
+
+        [Fact]
+        public void AddSurveyTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Equal(1, 1);
+        }
+    }
+}
---
+++ Planets/Models/FakeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Planets.Models
+{
+    public class FakeRepository : IRepository
+    {
+        private static readonly List<Survey> _surveys = new List<Survey>();
+
+        public List<Survey> Surveys => _surveys;
+
+        public void AddSurvey(Survey survey) => Surveys.Add(survey);
+    }
+}
